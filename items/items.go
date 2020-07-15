6299ec2e891550fb1f62c2d2873e2c88a097cf93
Yevhen Shymotiuk <yevhenshymotiuk@gmail.com>
2020-07-15 23:51:42 +0300

    Add documentation for items package

--- items/items.go
+++ items/items.go
@@ -1,5 +1,7 @@
+// Package items provides models for extracted data
 package items
 
+// Laptop provides laptop specifications
 type Laptop struct {
 	Processor Processor `json:"processor"`
 	RAM       RAM       `json:"ram"`
@@ -8,24 +10,29 @@ type Laptop struct {
 	Price     Price     `json:"price"`
 }
 
+// Processor provides processor specifications
 type Processor struct {
 	Series string `json:"series"`
 	Model  string `json:"model"`
 }
 
+// RAM provides RAM specifications
 type RAM struct {
 	Capacity int `json:"capacity"`
 }
 
+// GPU provides GPU specifications
 type GPU struct {
 	Model string `json:"model"`
 }
 
+// Drive provides drive specifications
 type Drive struct {
 	Type     string `json:"type"`
 	Capacity int    `json:"capacity"`
 }
 
+// Price provides min/max prices of the laptop
 type Price struct {
 	Min int `json:"min"`
 	Max int `json:"max"`
